65851a5aadce024049613c3f86ec2325b8ad254c
Jeswin <jeswinpk@agilehead.com>
2014-04-11 09:45:46 +0530

    content widget can take a content field or a string

--- server/src/app-lib/widgets/content.jsx
+++ server/src/app-lib/widgets/content.jsx
@@ -3,12 +3,18 @@ var React = require("react");
 
 exports.Content = React.createClass({
     render: function() {
-        content = this.props.content.formatContent();
+    
+        if (typeof this.props.content != "string")            
+            content = this.props.content.formatContent();
+        else
+            content = this.props.content;
+            
         return (
             <div className="content" dangerouslySetInnerHTML={{__html: content}}>
                 {this.props.children}                
             </div>
         );
+        
     }
 });
 
