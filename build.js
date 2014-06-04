7fc9cc5cde6a4423b0da8863869c482b5d7bace9
Jeswin <jeswinpk@agilehead.com>
2014-06-04 19:21:12 +0530

    Build references npm

--- build.js
+++ build.js
@@ -3,7 +3,7 @@
     
     var start = Date.now();
 
-    var foraBuild = require('../fora-build');
+    var foraBuild = require('fora-build');
     var spawn = foraBuild.tools.process.spawn({ log: function(data) { process.stdout.write(data); } });
 
     var optimist = require('optimist')
