0e5bceef8581fae68a905843c496e66bb4414de2
Jeswin Kumar <jeswinpk@agilehead.com>
2014-08-03 10:32:40 +0530

    WIP: Move to JS and Refactoring

---
+++ server/src/lib/netutils/index.js
@@ -0,0 +1,36 @@
+(function () {
+    "use strict";
+
+    var url = require('url'),
+        fs = require('fs-extra'),
+        gm = require('gm'),
+        thunkify = require('fora-node-thunkify'),
+        logger = require('./logger'),
+        randomizer = require('./randomizer'),
+        fsutils = require('./fsutils');
+
+    var exec = thunkify(require('child_process').exec);
+
+    var downloadImage = function*(imageUrl) {
+        var _;
+
+        var parseResult = url.parse(imageUrl);
+        var hostArr = parseResult.hostname.split('.');
+        var extension = parseResult.pathname.split('/').pop().split('.').pop();
+        var filename = randomizer.uniqueId(8) + "_" + Date.now() + "." + extension.toLowerCase();
+
+        if(['jpg', 'jpeg', 'png', 'gif', 'bmp'].indexOf(extension.toLowerCase()) !== -1) {
+            var filePath = fsutils.getFilePath("temp", filename);
+            var _curl = "curl --proto =http,https --proto-redir =http,https --max-filesize 5000000 " + imageUrl + " > " + filePath;
+            _ = yield* exec(_curl);
+            logger.log("Downloaded " + imageUrl + " to " + filePath);
+            return filePath;
+        } else {
+            logger.log("Cannot download image. Invalid file extension for " + imageUrl);
+        }
+    };
+
+    module.exports = {
+        downloadImage: downloadImage
+    };
+})();
