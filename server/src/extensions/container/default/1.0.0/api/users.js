8764a2df652d78e94655de75d4c8485bf87e83ce
Jeswin Kumar <jeswinpk@agilehead.com>
2014-11-18 20:03:19 +0530

    Moved code from controllers to models

--- server/src/extensions/container/default/1.0.0/api/users.js
+++ server/src/extensions/container/default/1.0.0/api/users.js
@@ -11,41 +11,9 @@
     var conf = services.get("configuration");
     var fileService = new FileService(conf);
 
+
     var create = function*() {
-        var typesService = services.get('typesService');
-        var parser = new Parser(this, typesService);
-
-        var user = new models.User(
-            {
-                username: yield* parser.body('username'),
-                credentialId: this.session.credentialId,
-                name: yield* parser.body('name'),
-                location: yield* parser.body('location'),
-                email: (yield* parser.body('email') || 'unknownthis.foraproject.org'),
-                about: yield* parser.body('about'),
-                lastLogin: Date.now()
-            }
-        );
-        user = yield* user.save();
-
-        //Move images to assets
-        var picture = {
-            src: yield* parser.body('picture_src'),
-            small: yield* parser.body('picture_small')
-        };
-
-        var copy = function*(sourcePath, destFilename) {
-            var srcPathArr = sourcePath.split('/');
-            var file = srcPathArr.pop();
-            var subdir = srcPathArr.pop();
-            var source = fileService.getFilePath('images', subdir, file);
-            var dest = fileService.getFilePath('assets', user.assets, destFilename);
-            _ = yield* fileService.copyFile(source, dest);
-        };
-
-        _ = yield* copy(picture.src, user.username + ".jpg");
-        _ = yield* copy(picture.small, user.username + "_t.jpg");
-
+        var user = yield* models.User.createViaRequest(this);
         this.body = user.summarize();
     };
 
--- server/src/lib/fora-app-models/user.js
+++ server/src/lib/fora-app-models/user.js
@@ -7,15 +7,59 @@
         dataUtils = require('fora-data-utils'),
         userCommon = require('./user-common'),
         DbConnector = require('fora-app-db-connector'),
-        services = require('fora-app-services');
+        services = require('fora-app-services'),
+        FileService = require('fora-app-file-service'),
+        Parser = require('fora-request-parser');
 
+    var conf = services.get("configuration");
+    var fileService = new FileService(conf);
 
     var User = function(params) {
         dataUtils.extend(this, params);
     };
+
     userCommon.extendUser(User);
 
     var userStore = new DbConnector(User);
+
+
+    User.createViaRequest = function*(request) {
+        var typesService = services.get('typesService');
+        var parser = new Parser(request, typesService);
+
+        var user = new User({
+            username: yield* parser.body('username'),
+            credentialId: request.session.credentialId,
+            name: yield* parser.body('name'),
+            location: yield* parser.body('location'),
+            email: (yield* parser.body('email') || 'unknownthis.foraproject.org'),
+            about: yield* parser.body('about'),
+            lastLogin: Date.now()
+        });
+
+        user = yield* user.save();
+
+        //Move images to assets
+        var picture = {
+            src: yield* parser.body('picture_src'),
+            small: yield* parser.body('picture_small')
+        };
+
+        var copy = function*(sourcePath, destFilename) {
+            var srcPathArr = sourcePath.split('/');
+            var file = srcPathArr.pop();
+            var subdir = srcPathArr.pop();
+            var source = fileService.getFilePath('images', subdir, file);
+            var dest = fileService.getFilePath('assets', user.assets, destFilename);
+            _ = yield* fileService.copyFile(source, dest);
+        };
+
+        _ = yield* copy(picture.src, user.username + ".jpg");
+        _ = yield* copy(picture.small, user.username + "_t.jpg");
+
+        return user;
+    };
+
 
     User.prototype.save = function*() {
         if (!DbConnector.getRowId(this)) {
