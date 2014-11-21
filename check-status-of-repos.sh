98eeda954c9d7ebddb0f2910058a6d1c175e12e8
Jeswin Kumar <jeswinpk@agilehead.com>
2014-11-21 13:29:35 +0530

    scripts to check git status of related fora projects

---
+++ check-status-of-repos.sh
@@ -0,0 +1,24 @@
+#This checks if there are uncommitted files in any of the projects associated with fora
+
+check_status_of_repos() {
+    curdir=`pwd`
+    proj=$1
+    basedir=$2
+    cd $basedir/$proj
+    git status
+    cd $curdir
+}
+
+check_status_of_repos "fora-build", "node_modules"
+check_status_of_repos "fora-data-utils", "server/node_modules"
+check_status_of_repos "fora-db", "server/node_modules"
+check_status_of_repos "fora-db-backend-mongodb", "server/node_modules"
+check_status_of_repos "fora-extensions-service", "server/node_modules"
+check_status_of_repos "fora-request", "server/node_modules"
+check_status_of_repos "fora-request-parser", "server/node_modules"
+check_status_of_repos "fora-router", "server/node_modules"
+check_status_of_repos "fora-types-service", "server/node_modules"
+check_status_of_repos "fora-validator", "server/node_modules"
+check_status_of_repos "fora-build", "server/node_modules"
+check_status_of_repos "fora-build", "server/node_modules"
+check_status_of_repos "fora-build", "server/node_modules"
---
+++ get-repos-from-git.sh
@@ -0,0 +1,24 @@
+echo Fetching dev repos related to Fora
+
+get_repos_from_git() {
+    proj=$1
+    basedir=$2
+    if [ ! -d $basedir/$proj/.git ]; then
+        echo cloning $basedir/$proj
+        rm -rf $basedir/$proj
+        git clone https://github.com/jeswin/$proj $basedir/$proj
+    else
+        echo skipped $basedir/$proj
+    fi
+}
+
+get_repos_from_git "fora-build" "node_modules"
+get_repos_from_git "fora-data-utils" "server/node_modules"
+get_repos_from_git "fora-db" "server/node_modules"
+get_repos_from_git "fora-db-backend-mongodb" "server/node_modules"
+get_repos_from_git "fora-extensions-service" "server/node_modules"
+get_repos_from_git "fora-request" "server/node_modules"
+get_repos_from_git "fora-request-parser" "server/node_modules"
+get_repos_from_git "fora-router" "server/node_modules"
+get_repos_from_git "fora-types-service" "server/node_modules"
+get_repos_from_git "fora-validator" "server/node_modules"
