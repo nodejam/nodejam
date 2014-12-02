06e6b58dcf293b359213a682fcf24f64a56aae0a
Jeswin Kumar <jeswinpk@agilehead.com>
2014-12-02 22:26:35 +0530

    fixing siteoptions

--- server/src/lib/fora-app-ui/controls/site-options.jsx
+++ server/src/lib/fora-app-ui/controls/site-options.jsx
@@ -10,7 +10,6 @@
                     <ul>
                         <li><a href="/"><i className="fa fa-home"></i>Home</a></li>
                         <li><a href="/apps"><i className="fa fa-list"></i>Forums</a></li>
-                        <li className="account"></li>
                     </ul>
                     <div className="transparent-overlay" onClick={this.props.closeHandler}>
                     </div>
