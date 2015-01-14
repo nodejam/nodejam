978a35ab588caeb69b018b2ed5a8e3fcf7304d34
Jeswin Kumar <jeswinpk@agilehead.com>
2015-01-14 10:55:20 +0530

    New scripts to make things easier

---
+++ git-push-external-repos.sh
@@ -0,0 +1,23 @@
+#This checks if there are uncommitted files in any of the projects associated with fora
+
+git_push() {
+    curdir=`pwd`
+    proj=$1
+    basedir=$2
+    echo checking $basedir/$proj
+    cd $basedir/$proj
+    git push origin master
+    cd $curdir
+    echo
+}
+
+git_push "crankshaft" "node_modules"
+git_push "fora-data-utils" "node_modules"
+git_push "ceramic" "node_modules"
+git_push "ceramic-db" "node_modules"
+git_push "ceramic-backend-mongodb" "node_modules"
+git_push "fora-extensions-service" "node_modules"
+git_push "fora-request" "node_modules"
+git_push "fora-request-parser" "node_modules"
+git_push "fora-router" "node_modules"
+git_push "nodefunc-promisify" "node_modules"
---
+++ npm-publish-external-repos.sh
@@ -0,0 +1,23 @@
+#This checks if there are uncommitted files in any of the projects associated with fora
+
+npm_publish() {
+    curdir=`pwd`
+    proj=$1
+    basedir=$2
+    echo checking $basedir/$proj
+    cd $basedir/$proj
+    npm publish
+    cd $curdir
+    echo
+}
+
+npm_publish "crankshaft" "node_modules"
+npm_publish "fora-data-utils" "node_modules"
+npm_publish "ceramic" "node_modules"
+npm_publish "ceramic-db" "node_modules"
+npm_publish "ceramic-backend-mongodb" "node_modules"
+npm_publish "fora-extensions-service" "node_modules"
+npm_publish "fora-request" "node_modules"
+npm_publish "fora-request-parser" "node_modules"
+npm_publish "fora-router" "node_modules"
+npm_publish "nodefunc-promisify" "node_modules"
