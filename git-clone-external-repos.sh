4fbca37ae821b269c17b577bec6cf7ce151690a5
Jeswin <jeswinpk@agilehead.com>
2015-05-11 10:34:32 +0530

    Add new repositories to helper bash scripts

--- git-clone-external-repos.sh
+++ git-clone-external-repos.sh
@@ -29,6 +29,7 @@ runtask "ceramic-backend-nedb" "node_modules/fora-template-blog/node_modules"
 runtask "isotropy-browser-mode" "node_modules/fora-template-blog/node_modules";
 runtask "isotropy" "node_modules/fora-template-blog/node_modules/isotropy-browser-mode/node_modules";
 runtask "isotropy-koa-context-in-browser" "node_modules/fora-template-blog/node_modules/isotropy-browser-mode/node_modules";
+runtask "isotropy-request-response-in-browser" "node_modules/fora-template-blog/node_modules/isotropy-browser-mode/node_modules/isotropy-koa-context-in-browser/node_modules";
 
 runtask "isotropy-koa-mode" "node_modules/fora-template-blog/node_modules";
 runtask "isotropy" "node_modules/fora-template-blog/node_modules/isotropy-koa-mode/node_modules";
@@ -36,3 +37,4 @@ runtask "isotropy" "node_modules/fora-template-blog/node_modules/isotropy-koa-mo
 runtask "isotropy-dev-mode" "node_modules/fora-template-blog/node_modules";
 runtask "isotropy" "node_modules/fora-template-blog/node_modules/isotropy-dev-mode/node_modules";
 runtask "koa-in-browser" "node_modules/fora-template-blog/node_modules/isotropy-dev-mode/node_modules";
+runtask "isotropy-request-response-in-browser" "node_modules/fora-template-blog/node_modules/isotropy-dev-mode/node_modules/koa-in-browser/node_modules";
--- git-push-external-repos.sh
+++ git-push-external-repos.sh
@@ -23,7 +23,8 @@ runtask "ceramic-backend-nedb" "node_modules/fora-template-blog/node_modules"
 
 runtask "isotropy-browser-mode" "node_modules/fora-template-blog/node_modules";
 runtask "isotropy" "node_modules/fora-template-blog/node_modules/isotropy-browser-mode/node_modules";
-runtask "isotropy-browser-request" "node_modules/fora-template-blog/node_modules/isotropy-browser-mode/node_modules";
+runtask "isotropy-koa-context-in-browser" "node_modules/fora-template-blog/node_modules/isotropy-browser-mode/node_modules";
+runtask "isotropy-request-response-in-browser" "node_modules/fora-template-blog/node_modules/isotropy-browser-mode/node_modules/isotropy-koa-context-in-browser/node_modules";
 
 runtask "isotropy-koa-mode" "node_modules/fora-template-blog/node_modules";
 runtask "isotropy" "node_modules/fora-template-blog/node_modules/isotropy-koa-mode/node_modules";
@@ -31,3 +32,4 @@ runtask "isotropy" "node_modules/fora-template-blog/node_modules/isotropy-koa-mo
 runtask "isotropy-dev-mode" "node_modules/fora-template-blog/node_modules";
 runtask "isotropy" "node_modules/fora-template-blog/node_modules/isotropy-dev-mode/node_modules";
 runtask "koa-in-browser" "node_modules/fora-template-blog/node_modules/isotropy-dev-mode/node_modules";
+runtask "isotropy-request-response-in-browser" "node_modules/fora-template-blog/node_modules/isotropy-dev-mode/node_modules/koa-in-browser/node_modules";
